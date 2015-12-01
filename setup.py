1c2253d42e1d81947d357619a4fa3f8a08451399
Nikita Kuznetsov <me@daedalus.ru>
2015-12-01 22:51:36 +0300

    correct license in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'
