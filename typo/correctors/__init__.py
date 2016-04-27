366dbb6bab434002276cae9d6da075f8a953af4e
Harut <yes@harutune.name>
2016-04-27 17:58:20 +0300

    updated typo corrector, mode for json answers

--- typo/correctors/__init__.py
+++ typo/correctors/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from simple import TypoDefault
-from utils import register_typo
-from utils import TYPO_CLASSES
+from .default import TypoDefault
+from .utils import register_typo
+from .utils import TYPO_CLASSES
 
 
 __all__ = ['TypoDefault', 'register_typo', 'TYPO_CLASSES']
